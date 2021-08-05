9298e17fa8d3b5872c0908e29d777f138bf93894
Sergey Privalov <SergeyPrivalof@yandex.ru>
2021-08-05 17:04:24 +0500

    1

--- js/main.js
+++ js/main.js
@@ -1,26 +1,62 @@
-window.addEventListener('DOMContentLoaded', () => {           // Ждём загрузки DOM дерева 
+window.addEventListener('DOMContentLoaded', () => { // Ждём загрузки DOM дерева 
   'use strict';
-  
-  // Плавный скролл до якоря
-  const anchors = () => {
-    const anchors = document.querySelectorAll('a[href*="#"]');
-  
-    for (let anchor of anchors) {
-      anchor.addEventListener('click', (e) => {
-        e.preventDefault();
-  
-        const blockID = anchor.getAttribute('href').substr(1);
-  
-        document.getElementById(blockID).scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-        });
-        
-      });
-    }
-  };
-  //anchors();
 
-  
+  const body = document.body;
+
+  const progress = () => {
+    const progress = document.querySelector('.gains-progress');
+    const more = document.querySelector('.gains-more');
+    const gains = document.querySelector('.gains');
+
+    
+    progress.addEventListener('click', () => {
+      let text = progress.textContent;
+
+      if(!more.classList.contains('active')){
+        more.classList.add('active');
+        progress.textContent = progress.textContent.replace('Прогресс достижений', 'Свернуть');
+      } else {
+        more.classList.remove('active');
+        progress.textContent = progress.textContent.replace('Свернуть', 'Прогресс достижений');
+      }
+
+    })
+
+  }
+  progress();
+
+  const addClass = () => {
+    const elem = document.querySelector('.bell__container');
+    body.addEventListener('click', (e) => {
+      let target = e.target;
+
+      // Появление блока оповещения
+      if(target.closest('#bell')){
+        elem.classList.toggle('active');
+      } else {
+        elem.classList.remove('active');
+      }
+
+      if(target.closest('.bell__container')){
+        elem.classList.add('active')
+      }
+
+      // Меню
+
+      if(target.closest('.cls')) {
+        
+      }
+      
+    })
+  };
+  addClass();
+
+  const menu = () => {
+    const btn = document.querySelector('.cls');
+
+
+
+  };
+  menu();
 
 });
