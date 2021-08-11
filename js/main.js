022e3fb8c1898ff1ac6bc7f2796b87179b509791
Sergey Privalov <SergeyPrivalof@yandex.ru>
2021-08-11 14:52:28 +0500

    Свёрстаны и адаптированы История контрактов и История заявок

--- js/main.js
+++ js/main.js
@@ -88,8 +88,8 @@ window.addEventListener('DOMContentLoaded', () => { // Ждём загрузки
 
       // Закрывает select если кликнули мимо
       const selectSingle = document.querySelector('.__select');
-      if(selectSingle) {
-        if (!target.closest('.__select__content') && !target.closest('.__select__title')){
+      if (selectSingle) {
+        if (!target.closest('.__select__content') && !target.closest('.__select__title')) {
           selectSingle.setAttribute('data-state', '');
         }
       }
@@ -101,12 +101,12 @@ window.addEventListener('DOMContentLoaded', () => { // Ждём загрузки
     const houseContent = document.querySelector('.house-content');
     const contentHeader = document.querySelector('.content__header').clientHeight;
 
-    if(houseContent) {
+    if (houseContent) {
       houseContent.style.cssText = `
         height: calc( 100% - ${contentHeader}px)
       `
     }
-  }
+  };
   houseContent()
 
   const addMarg = () => {
@@ -122,17 +122,17 @@ window.addEventListener('DOMContentLoaded', () => { // Ждём загрузки
         height: calc( 100% - ${contentHeader}px);
       `
     }
-  }
+  };
   addMarg()
 
   const select = () => {
     const selectBox = document.querySelector('.select-box');
     const selectSingle = document.querySelector('.__select');
-    
-    if(selectBox && selectSingle) {
+
+    if (selectBox && selectSingle) {
       const selectSingle_title = selectSingle.querySelector('.__select__title');
       const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
-      
+
       // Toggle menu
       selectSingle_title.addEventListener('click', () => {
         if ('active' === selectSingle.getAttribute('data-state')) {
@@ -141,7 +141,7 @@ window.addEventListener('DOMContentLoaded', () => { // Ждём загрузки
           selectSingle.setAttribute('data-state', 'active');
         }
       });
-  
+
       // Close when click to option
       for (let i = 0; i < selectSingle_labels.length; i++) {
         selectSingle_labels[i].addEventListener('click', (evt) => {
@@ -149,10 +149,80 @@ window.addEventListener('DOMContentLoaded', () => { // Ждём загрузки
           selectSingle.setAttribute('data-state', '');
           selectSingle_title.classList.add('act');
         });
-      } 
-      
+      }
+
     }
-  }
+  };
   select();
+
+  // Расчёт высоты блока табов
+  const tabHeight = () => {
+    const heightWindow = window.innerHeight,
+      tabBlock = document.querySelectorAll('.box-overflow');
+
+    tabBlock.forEach((item) => {
+      let tabTop = item.offsetTop;
+      let blockHeight = heightWindow - tabTop;
+      item.style.cssText = `
+        max-height: ${blockHeight - 6}px;
+      `
+    })
+
+
+  };
+  tabHeight();
+
+
+  // Табы
+  const tabs = () => {
+    const tabHeader = document.querySelector('.contract-header'),
+      tabs = tabHeader.querySelectorAll('.contract-header__tab'),
+      heightWindow = window.innerHeight,
+      tabContent = document.querySelectorAll('.contract-tab'),
+      contractTop = document.querySelector('.contract').offsetTop,
+      contractTab = document.querySelectorAll('.contract-tab__box'),
+      tabHeaderTop = tabHeader.offsetHeight;
+
+    let height = heightWindow - contractTop - tabHeaderTop - 30;
+
+    if (tabHeader) {
+      contractTab.forEach((i) => {
+        i.style.cssText = `
+          height: 100%;
+          overflow-y: auto;
+          max-height: ${height}px;
+          `
+      })
+
+      const toggleTabContent = (index) => {
+        for (let i = 0; i < tabContent.length; i++) {
+          if (index === i) {
+            tabs[i].classList.add('active');
+            tabContent[i].classList.remove('active-none');
+            tabContent[i].classList.add('active');
+          } else {
+            tabs[i].classList.remove('active');
+            tabContent[i].classList.remove('active');
+            tabContent[i].classList.add('active-none');
+          }
+        }
+      };
+
+      tabHeader.addEventListener('click', (e) => {  
+
+        let target = e.target;
+        if (target.classList.contains('contract-header__tab')) {
+          tabs.forEach((item, i) => {
+            if (item === target) {
+              toggleTabContent(i);
+            }
+          });
+        }
+
+
+      })
+    }
+  };
+  tabs();
 
 });
